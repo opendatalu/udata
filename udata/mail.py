23714cd88b229865a5dbf049a64c76271090c17c
Jean-François Kéner <JJL762@gouv.etat.lu>
2022-07-25 11:19:33 +0200

    Fix make_msgid domain to data.public.lu

--- udata/mail.py
+++ udata/mail.py
@@ -4,7 +4,16 @@ from contextlib import contextmanager
 from blinker import signal
 
 from flask import current_app, render_template
-from flask_mail import Mail, Message
+from flask_mail import Mail, Message as _Message
+
+from email.utils import make_msgid
+class Message(_Message):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # RFC 2822 does not allow over 78 long Message-ID header or it flags as spam.
+        # AWS servers generate way too large hostnames.
+        # flask_mail has not method to pass the domain argument to make_msgid, so we have to overload the Message class
+        self.msgId = make_msgid(domain="data.public.lu") # Improvement: set from a variable in udata.cfg
 
 from udata import i18n
 

3043323cc3591f58c71b217add16d233015656f9
Jean-François Kéner <JJL762@gouv.etat.lu>
2023-01-27 13:46:08 +0100

    Hardcode and log sender

--- udata/auth/mails.py
+++ udata/auth/mails.py
@@ -1,7 +1,10 @@
+import logging
 import email_validator
 from flask import current_app
 from udata.tasks import task
 from udata.patch.mail import Message
+
+log = logging.getLogger(__name__)
 
 
 @task
@@ -15,7 +18,14 @@ def sendmail(msg):
 
 class UdataMailUtil:
     """
-    A class which is used by the flask-security Plug-in.
+    A rework of the udata-security MailUtil passed on flask-security unit
+    send_mail is called by flask-security
+    https://github.com/Flask-Middleware/flask-security/blob/master/flask_security/mail_util.py
+
+    The sender is in the udata.cfg as SECURITY_EMAIL_SENDER
+    This variable name is (?) passed to flask-security
+    https://flask-security-too.readthedocs.io/en/stable/configuration.html#SECURITY_EMAIL_SENDER
+
     Other mails may be sent by ../mail.py 
     """
 
@@ -28,6 +38,11 @@ class UdataMailUtil:
             sender = (str(sender[0]), str(sender[1]))
         else:
             sender = str(sender)
+
+        # sender at this point seems problematic, add debug and overwrite it for now
+        log.info("Send mail to %s" % str(sender))
+        sender = self.app.config.get('MAIL_DEFAULT_SENDER', ('Portail Open Data', 'info@data.public.lu'))
+
         msg = Message(subject, sender=sender, recipients=[recipient])
         msg.body = body
         msg.html = html
